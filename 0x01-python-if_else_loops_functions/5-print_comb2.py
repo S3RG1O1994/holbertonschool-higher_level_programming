821e75ef156027c072b877d3eb87ded392e01b24
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 19:00:40 -0400

    [Fix]: if additinal for the comma and space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for w in range(100):
-    print('{:02d},'.format(w), end="")
+    if w == 99:
+        print('{:02d}'.format(w))
+    else:
+        print('{:02d}, '.format(w), end="")
