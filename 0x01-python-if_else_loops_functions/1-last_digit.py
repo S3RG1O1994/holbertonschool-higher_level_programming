86cba70f338f938c73e499b9ea7dfcfb8f60daee
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 16:57:32 -0400

    [Fix]: distance : in sentences

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 last = 0
-if number < 0 :
+if number < 0:
     last = (number * -1) % 10 * -1
-else :
+else:
     last = number % 10
-if last == 0 :
+if last == 0:
     print('Last digit of {} is {} and is 0'.format(number, last))
-elif last > 5 :
+elif last > 5:
     print('Last digit of {} is {} and is greater than 5'.format(number, last))
-elif last < 6 :
+elif last < 6:
     print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last))
