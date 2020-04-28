5eb7142a8215f10c015c4b1ae8cf22eb92ac84dd
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 16:39:08 -0400

    [Fix]: Quit space in line 8, 6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last = 0
 if number < 0 :
-    last = (number * -1) % 10 
+    last = (number * -1) % 10
 else :
     last = number % 10
-    
 if last == 0 :
     print('Last digit of {} is {} and is 0'.format(number, last))
 elif last > 5 :
