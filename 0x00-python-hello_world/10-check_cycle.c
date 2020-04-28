d04f44622d896c43428029363bf71468d0342119
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 00:52:12 -0400

    [Fix]: betty-style

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,27 +3,25 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_cycle - find loops in the linked list
+ * @list: pointer to head of list
+ * Return: 1 if exists loop or 0 if not exists loop
  */
-
 int check_cycle(listint_t *list)
 {
         listint_t *first_list = list;
         listint_t *second_list = list;
 
         if (!list->next || !list)
-                return(0);
-
+                return (0);
         first_list = first_list->next;
         second_list = second_list->next->next;
-
-        while(first_list && second_list && second_list)
+        while (first_list && second_list && second_list->next)
         {
                 if (first_list == second_list)
-                        return(1);
+                        return (1);
                 first_list = first_list->next;
                 second_list = second_list->next->next;
         }
+        return (0);
 }
