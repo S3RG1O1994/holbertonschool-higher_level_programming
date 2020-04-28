346948732fef284f8ee039e088e07f0a8ef2aa76
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 19:43:33 -0400

    [Add]: New excercise

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for w in range(0,9): 
+    for i in range(10):
+        if  i <= w :
+            continue
+        else:
+            if w < 8:
+                print('{}{}, '.format(w, i), end="")
+            else:
+                print('{}{}'.format(w, i))
