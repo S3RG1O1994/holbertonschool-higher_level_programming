802fa6fe7519ccb8778969c1e50fe4e1ed74aba5
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 20:57:46 -0400

    [Add]: New excercise

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        copy = ord(str[i])
+        if (copy > 96 and copy < 123):
+            copy -= 32
+            copy = chr(copy)
+            print(copy, end="")
+        else:
+            copy = chr(copy)
+            print(copy, end="")
+    print()
