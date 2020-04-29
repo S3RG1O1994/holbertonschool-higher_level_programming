8220b6692dc491bfd64f67a961fa650919834462
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-28 22:45:53 -0400

    [FIx]: Space last line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ if number < 0:
 else:
     last = number % 10
 print('Last digit of {} is {} and is '.format(number, last), end="")
-if last == 0:
-    print('0')
-elif last > 5:
+if last > 5:
     print('and is greater than 5')
+elif last == 0:
+    print('0')
 else:
     print('and is less than 6 and not 0')
