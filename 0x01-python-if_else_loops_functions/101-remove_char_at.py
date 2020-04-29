7114bbe84c90cc402335975d7e28b549d4bd69a9
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-29 14:22:00 -0400

    [Add]: Advanced task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    copy = ""
+    for i in range(len(str)):
+        if i != n:
+            copy += str[i]
+        else:
+            continue
+    return copy
