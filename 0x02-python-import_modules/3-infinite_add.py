cf1dc31efd36c9c1f337d8564c349281d10864c9
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-30 18:00:20 -0400

    [Add]: inifinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print('{}'.format(len(argv) - 1))
+    else:
+        aux = 0 
+        for i in range(1, len(argv)):
+            aux += int(argv[i])
+        print(aux)
