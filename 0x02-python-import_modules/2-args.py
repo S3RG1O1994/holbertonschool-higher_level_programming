23556f7e248868402961b90e21bda7f58654c3ba
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-30 17:34:08 -0400

    [Fix]: Fix print when number arguments is 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 if __name__ == '__main__':
     if len(argv) == 1:
-        print('{} argument.'.format(len(argv) - 1))
+        print('{} arguments.'.format(len(argv) - 1))
     else:
         if len(argv) == 2:
             print('{} argument:'.format(len(argv) - 1))
