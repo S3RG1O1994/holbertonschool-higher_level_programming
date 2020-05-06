3e78d3a7c991d36bfba6ba59c55d1421c2db71cd
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-05 20:26:07 -0400

    [Fix]: New conditionals

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in matrix[i]:
-            if j != len(matrix[i]) and i == 0:
+    for i in matrix:
+        for j in i:
+            if j < len(matrix):
                 print('{:d}'.format(j), end="")
-            elif i > 0 and j != len(matrix[i]) * 2:
+            elif j < len(matrix) * 2:
                 print('{:d}'.format(j), end="")
             else:
-                print('{:d}'.format(j))
-    print()
+                print('{:d}'.format(j), end="")
+        print()
