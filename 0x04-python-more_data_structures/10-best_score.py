d13c6db3c9d59f57d87624b7c2ecc2f0cb8f07e7
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-07 21:50:59 -0400

    [Add]: New excercise

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    new_value = a_dictionary.values()
-    sum = 0
-    for value in new_value:
-        if sum < value:
-            sum = value
-    return sum
+    else:
+        new_value = a_dictionary.values()
+        sum = 0
+        for value in new_value:
+            if sum < value:
+                sum = value
+        return sum
