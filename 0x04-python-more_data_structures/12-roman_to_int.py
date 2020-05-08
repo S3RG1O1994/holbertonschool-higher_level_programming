c208972a8f25089badb1c61a03461b2983546fe6
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-08 00:18:59 -0400

    [Fix]: long characters

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str and roman_string is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     num = 0
     num_romans = {
