fc5ed7bfaf696aa2526743e1b7b3faf7f3247031
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-08 00:17:17 -0400

    [Fix]: long characters

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != int and roman_string != None:
-        num = 0
-        num_romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        for pos in range(len(roman_string)):
-            if pos == len(roman_string) - 1:
-                num += num_romans[roman_string[pos]]
-            elif num_romans[roman_string[pos]] >= num_romans[roman_string[pos + 1]]:
-                num += num_romans[roman_string[pos]]
-            else:
-                num -= num_romans[roman_string[pos]]
-        return num
-    else:
+    if type(roman_string) != str and roman_string is None:
         return 0
+    num = 0
+    num_romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+    for i in range(len(roman_string)):
+        if i == len(roman_string) - 1:
+            num += num_romans[roman_string[i]]
+        elif num_romans[roman_string[i]] >= num_romans[roman_string[i + 1]]:
+            num += num_romans[roman_string[i]]
+        else:
+            num -= num_romans[roman_string[i]]
+    return num
