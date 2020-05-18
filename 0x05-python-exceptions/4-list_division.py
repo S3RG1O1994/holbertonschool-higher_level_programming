f8adb69ecc1093f30dcc4331d9790f62088e5aa3
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 18:04:51 -0500

    [Fix] adition variable for management result of the division within two list - variable -> item

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,16 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            new_list.append(my_list_1[i] / my_list_2[i])
+            item = my_list_1[i] / my_list_2[i]
         except IndexError:
             print('Out for range')
-            new_list.append(0)
+            item = 0
         except ZeroDivisionError:
             print('division by 0')
-            new_list.append(0)
+            item = 0
         except TypeError:
             print ('wrong type')
-            new_list.append(0)
+            item = 0
         finally:
-            if len(new_list) == list_length:
-                return new_list
+            new_list.append(item)
+    return new_list
