82f46bc37874c22e30cc8f6bd97f8a66c4b4ffb7
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 18:11:56 -0500

    [Fix]: Corrections in string of IndexError

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             item = my_list_1[i] / my_list_2[i]
         except IndexError:
-            print('Out for range')
+            print('out for range')
             item = 0
         except ZeroDivisionError:
             print('division by 0')
