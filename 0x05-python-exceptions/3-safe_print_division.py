b6e5bee53bc2e3e95fa54533b771e43cf4f4703d
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 15:55:01 -0500

    [Add]: Fourth exersice

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,11 +2,10 @@
 def safe_print_division(a, b):
 	try:
 		tmp = a / b
-		return tmp
 	except ZeroDivisionError:
 		tmp = None
 		print('Inside result: None')
-		return tmp
 	finally:
 		if tmp:
 			print('Inside result: {}'.format(tmp))
+		return tmp
