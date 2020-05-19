92551f417ef42a9a19d4fab7c65f80fc350b45fc
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-19 17:18:56 -0500

    [Add] documentation of class and function constructor

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""Define instant of one object square with private attribute
+    Size is have validate(type data/ Value data)
+
+Raises:
+    TypeError: [When type of data within size is not int]
+    ValueError: [When size is less that Zero]
+"""
+
+
 class Square:
-	def __init__(self, size=0):
-		if type(size) is not int:
-			raise TypeError('size must be an integer')
-		if size < 0:
-			raise ValueError('size must be >= 0')
-		self.__size = size
+    """class constructor"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
