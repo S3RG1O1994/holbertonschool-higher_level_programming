60be6aedc55a860b4f244d399b2a1be1f199c410
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 19:49:33 -0500

    [Fix]: pep8 style

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-		tmp = a / b
-	except ZeroDivisionError:
-		tmp = None
-	finally:
-		print('Inside result: {}'.format(tmp))
-		return tmp
+    try:
+        tmp = a / b
+    except ZeroDivisionError:
+        tmp = None
+    finally:
+        print('Inside result: {}'.format(tmp))
+        return tmp
