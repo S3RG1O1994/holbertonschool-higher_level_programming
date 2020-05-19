56504e65a801c356e572d54cd1c7be7dc2d267de
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 21:57:46 -0500

    [Fix]: import stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    from sys import stderr
     try:
+        from sys import stderr
         print('{:d}'.format(value))
         return True
     except Exception as error:
