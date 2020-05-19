7f8e3ba37bbbacf89f67bab933285c57d3351ab0
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-18 21:53:21 -0500

    [Add]: Advanced task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/udr/bin/python3
+from sys import stderr
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as error:
+        print('Exception: {:s}'.format(str(error)), file=stderr)
+        return None
