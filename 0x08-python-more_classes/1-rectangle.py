6bdab3d111c030e790e12f2b1937c9e00712f03a
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-25 20:05:14 -0500

    [Fix]: Corrections in conditionals

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,46 +2,44 @@
 """class for create an object called rectangle, with an width and height gives.
 
 Raises:
-    TypeError: [width must be an integer]
-    ValueError: [width must be >= 0]
-    TypeError: [height must be an integer]
-    ValueError: [height must be >= 0]
+	TypeError: [width must be an integer]
+	ValueError: [width must be >= 0]
+	TypeError: [height must be an integer]
+	ValueError: [height must be >= 0]
 
 Returns:
-    [attribute] -- [the attribute private for change his value]
+	[attribute] -- [the attribute private for change his value]
 """
-
-
 class Rectangle:
-    """Class constructor"""
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+	"""Class constructor"""
+	def __init__(self, width=0, height=0):
+		self.__width = width
+		self.__height = height
 
-    """class getter"""
-    @property
-    def width(self):
-        return self.width
+	"""class getter"""
+	@property
+	def width(self):
+		return self.__width
 
-    """Class setter"""
-    @width.setter
-    def width(self, value):
-        if value is int:
-            raise TypeError('width must be an integer')
-        elif value <= 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
+	"""Class setter"""
+	@width.setter
+	def width(self, value):
+		if type(value) != int:
+			raise TypeError('width must be an integer')
+		elif value <= 0:
+			raise ValueError('width must be >= 0')
+		self.__width = value
 
-    """class getter"""
-    @property
-    def height(self):
-        return self.height
+	"""class getter"""
+	@property
+	def height(self):
+		return self.__height
 
-    """Class setter"""
-    @height.setter
-    def height(self, value):
-        if value is int:
-            raise TypeError('height must be an integer')
-        elif value <= 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
+	"""Class setter"""
+	@height.setter
+	def height (self, value):
+		if type(value) != int:
+			raise TypeError('height must be an integer')
+		elif value <= 0:
+			raise ValueError('height must be >= 0')
+		self.__height = value
