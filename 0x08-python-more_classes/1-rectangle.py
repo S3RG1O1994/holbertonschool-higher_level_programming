adf61087bbcede19ed02f4b99fd7cdf4479a3c2c
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-25 20:08:44 -0500

    [Add]: Corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ Returns:
 class Rectangle:
 	"""Class constructor"""
 	def __init__(self, width=0, height=0):
-		self.__width = width
-		self.__height = height
+		self.width = width
+		self.height = height
 
 	"""class getter"""
 	@property
