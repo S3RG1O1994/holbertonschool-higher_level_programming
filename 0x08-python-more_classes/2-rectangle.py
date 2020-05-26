cf34fa46926e6bbc9d65bf008b30763bae0749f3
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-25 20:28:12 -0500

    [Fix] conditional in parameter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,4 +53,6 @@ class Rectangle:
 
     """method for print perimeter the object instances"""
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         return (self.width * 2) + (self.height * 2)
