139f40a7ad7f28608323263d816670ed2e46a4e9
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-02 21:43:57 -0500

    [Fix]: Correction-exceptions

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,30 +1,8 @@
 #!/usr/bin/python3
-"""[class bassGeometry contain base for anyfigure geometric]
-
-Raises:
-    Exception: [area() is not implemented]
-    TypeError: [<name> must be an integer]
-    ValueError: [<name> must be greater than 0]
-"""
-
-
-class BaseGeometry:
-    """[function for find area]
-    """
-
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    """[function for validate type or quantity of the value]
-    """
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
 """[class Rectangle is for create an object]
 """
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
