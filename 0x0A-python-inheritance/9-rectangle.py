471f48804938fe882a2a8adf36efb69b6d993e2a
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-02 21:56:41 -0500

    [Add]: fixed type data in format

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -39,8 +39,8 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     """[edit str method for this method]
