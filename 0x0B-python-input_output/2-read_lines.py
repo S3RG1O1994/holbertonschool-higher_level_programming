e4551b5c9d262c6c85c3d8e5c67f7e7e3e4bf692
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-03 22:12:01 -0500

    [fix]: pep8-style

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,8 +7,10 @@ def read_lines(filename="", nb_lines=0):
     """[Function for read an number of lines]"""
 
     with open(filename, mode='r', encoding='utf-8') as f:
-        if nb_lines <= 0:
+        lines = f.readlines()
+        if nb_lines <= 0 or nb_lines >= len(lines):
             print(f.read())
         else:
-            for i in range(nb_lines):
-                print(f.readline())
+            lines = lines[0: nb_lines]
+            for i in lines:
+                print(i)
