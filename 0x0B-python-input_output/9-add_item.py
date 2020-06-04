422c3f03c3b448721886c4db4babed010bd1958f
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-03 23:38:06 -0500

    [Fix]: styles

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-'''
-file: 9-add_item.py
-description: adds all arguments to a Python list, and then save them to a file
-'''
+"""Adds all arguments to a Python list, and then save them to a file."""
 from sys import argv
-from os import path
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
 
-arg_list = []
-if path.isfile(filename):
-    arg_list = load_from_json_file(filename)
+try:
+    sv = load("add_item.json")
+except FileNotFoundError:
+    sv = []
+for ct in argv[1:]:
+    sv.append(ct)
 
-for a in argv[1:]:
-    arg_list.append(a)
-
-save_to_json_file(arg_list, "add_item.json")
+save(sv, "add_item.json")
