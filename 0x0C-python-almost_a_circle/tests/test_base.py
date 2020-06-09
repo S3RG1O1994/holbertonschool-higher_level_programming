c70835b90c14fdb17338de0df2e8d1733ecf93dc
S3RG1O1994 <se.diaz60@gmail.com>
2020-06-09 17:49:21 -0500

    [Fix]: New test

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,23 +1,81 @@
 #!/usr/bin/python3
-
+"""Unittest for base.py Module"""
 import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 import json
-from models import base
-Base = base.Base
+import pep8
 
 
 class TestBase(unittest.TestCase):
-    """Testing functionality"""
 
-    def TestNoid(self):
-        """Testing id as None"""
-        self.assertEqual(Base().id, 1)
+    def test_Basic_Things(self):
+        self.b1 = Base(1)
+        self.b2 = Base(2)
+        self.b3 = Base(-3)
+        self.b4 = Base("Test.,/|<>")
+        self.b5 = Base("Hola")
+        self.b6 = Base(3.1)
+        self.b7 = Base(-23)
 
-    def TestSetid(self):
-        """Testing id as not None"""
-        self.assertEqual(Base(34).id, 34)
+    def test_correct_type(self):
+        b1 = Base()
+        self.assertTrue(type(b1) == Base)
 
-    def TestMoreArgs(self):
-        """Testing too many args"""
-        with self.assertRaises(TypeError):
-            Base(2, 1)
+    def test_nb_objects(self):
+        b0 = Base()
+        self.assertTrue(hasattr(b0, '_Base__nb_objects'))
+        Base._Base__nb_objects = 0
+
+    def test_class(self):
+        b0 = Base()
+        self.assertTrue(isinstance(b0, Base))
+
+    def test_Base_equal(self):
+        b1 = Base(13)
+        self.assertEqual(b1.id, 13)
+        b2 = Base(-13)
+        self.assertEqual(b2.id, -13)
+        b3 = Base("hola")
+        self.assertEqual(b3.id, "hola")
+        b4 = Base(13.1)
+        self.assertEqual(b4.id, 13.1)
+        b5 = Base(3)
+        self.assertEqual(b5.id, 3)
+        Base._Base__nb_objects = 0
+
+    def test_json_to_string_rec(self):
+        Rec = Rectangle(13, 1, 2, 8, 9)
+        dic = Rec.to_dictionary()
+        json_Dict = Base.to_json_string([dic])
+        self.assertEqual(type(json_Dict), str)
+
+    def test_json_to_string_sq(self):
+        Sq = Square(13, 1, 2, 8)
+        dic = Sq.to_dictionary()
+        json_Dict = Base.to_json_string([dic])
+        self.assertEqual(type(json_Dict), str)
+
+    def test_all_pep8_fl(self):
+        msg = "Found code style errors (and warning)."
+        style = pep8.StyleGuide(quiet=True)
+        fl_Base = "models/base.py"
+        fl_Base_tst = "tests/test_base.py"
+        fl_Rec = "models/rectangle.py"
+        fl_Rec_tst = "tests/test_rectangle.py"
+        fl_Sq = "models/square.py"
+        fl_Sq_tst = "tests/test_square.py"
+        check = style.check_files([fl_Rec, fl_Rec_tst, fl_Base, fl_Base_tst,
+                                   fl_Sq, fl_Sq_tst])
+        self.assertEqual(check.total_errors, 0, msg)
+
+    def test_save_to_file_rec(self):
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as fl:
+            self.assertEqual(fl.read(), '[]')
+
+    def test_save_to_file_sq(self):
+        Square.save_to_file([])
+        with open("Square.json") as fl:
+            self.assertEqual(fl.read(), '[]')
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,12 +1,56 @@
 #!/usr/bin/python3
-
+"""Test Rectangle Module"""
 import unittest
-from models import rectangle
-from models import base
-
-Rectangle = rectangle.Rectangle
-Base = base.Base
+from models.base import Base
+from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase):
-    """Check the funcionality of the rectangle"""
+class Test_Rectangle(unittest.TestCase):
+    """Test Rectangle with unittest."""
+
+    def test_Parameters(self):
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+
+    def test_area(self):
+        r1 = Rectangle(3, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r1.area(), 6)
+        self.assertEqual(r2.area(), 20)
+        self.assertEqual(r3.area(), 56)
+
+    def test_display(self):
+        r1 = Rectangle(1, 1)
+        self.assertTrue(hasattr(r1, 'display'))
+        Base._Base__nb_objects = 0
+
+    def test_str(self):
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(13, 13)
+        string_rep = r1.__str__()
+        self.assertEqual(string_rep, "[Rectangle] (1) 0/0 - 13/13")
+
+    def test_to_dictionary(self):
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(13, 3, 0, 1)
+        r1_dict = r1.to_dictionary()
+        self.assertEqual({"id": 1, "width": 13, "height": 3, "x": 0, "y": 1},
+                         r1_dict)
