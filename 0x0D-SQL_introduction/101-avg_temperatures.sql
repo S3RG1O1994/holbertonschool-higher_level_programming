ad0488ae25058df3805eda59887599d9fa1588dc
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-14 09:47:27 -0500

    [Fix]: 101-avg_temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- New task
-SELECT city, AVG(values) AS avg_temp FROM hbtn_0c_0 GROUP BY city ORDER BY avg_temp DESC;
+SELECT city,AVG(values) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
