228dfd3642099f88bcabff4ff72ca0ea054d6bf1
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-14 16:37:33 -0500

    [Add]: New excercise

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT tv_genres.name, count(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres
+SELECT tv_genres.name AS genre, count(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
