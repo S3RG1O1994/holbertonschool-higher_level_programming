b006030ab533bcbc1ac21d153fd415cf4e8dd3b1
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-08 17:47:15 -0500

    [Add]: New excercise

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,17 +6,16 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-	Uname = sys.argv[1]
-	Upasswd = sys.argv[2]
-	DB = sys.argv[3]
-	searched = sys.argv[4]
-	db = MySQLdb.connect(user=Uname, passwd=Upasswd, db=DB)
-	pointer = db.cursor()
-	query = ("SELECT * FROM states WHERE name LIKE %s\
-			ORDER BY states.id", (searched, ))
-	pointer.execute(query)
-	row = pointer.fetchall()
-	for items in row:
-			print(items)
-	pointer.close()
-	db.close()
+    Uname = sys.argv[1]
+    Upasswd = sys.argv[2]
+    DB = sys.argv[3]
+    searched = sys.argv[4]
+    db = MySQLdb.connect(user=Uname, passwd=Upasswd, db=DB)
+    pointer = db.cursor()
+    pointer.execute("SELECT * FROM states WHERE name LIKE %s\
+            ORDER BY states.id", (searched, ))
+    row = pointer.fetchall()
+    for items in row:
+            print(items)
+    pointer.close()
+    db.close()
