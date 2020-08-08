2ce9766de3dd04b8a064877ae6f17eadb476f9d1
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-08 17:35:38 -0500

    [Add]: New excercise

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Script for query all states in database.
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+	Uname = sys.argv[1]
+	Upasswd = sys.argv[2]
+	DB = sys.argv[3]
+	searched = sys.argv[4]
+	db = MySQLdb.connect(user=Uname, passwd=Upasswd, db=DB)
+	pointer = db.cursor()
+	query = 'SELECT * FROM states WHERE name LIKE %s\
+			ORDER BY states.id, (searched, )'
+	pointer.execute(query)
+	row = pointer.fetchall()
+	for items in row:
+			print(items)
+	pointer.close()
+	db.close()
