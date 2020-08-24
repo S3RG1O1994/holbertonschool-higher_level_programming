c2532dca10fd6e67299e82eec965a26c6335eaec
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-24 16:10:18 -0500

    [Change]: import all module request in urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 import urllib.request
 '''This function is for print status of an site-web'''
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        html = res.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        the_page = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
