2cb42eec8c3f46fe570e5e595d412b4bbd9d8bca
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-24 18:48:27 -0500

    [Fix]: script is executable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 '''This function is print response with id of the site-web'''
+
 from urllib import request, error
 import sys
 
 
 if __name__ == '__main__':
-    req = request.Request(sys.argv[1])
     try:
-        with request.urlopen(req) as response:
+        with request.urlopen(sys.argv[1]) as response:
             body = response.read()
             print(body.decode('utf-8'))
     except error.HTTPError as e:
