b0d88178278e57033f709b5928d75c90faab2c62
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-24 17:39:33 -0500

    [Add]: New excercise for printemail send by post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This function is print response with id of the site-web'''
+from urllib import request, parse 
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = parse.urlencode({'email' : sys.argv[2]})
+    email = email.encode('ascii')
+    request = request.Request(url, email)
+    with request.urlopen(request) as response:
+        the_page = response.read
+        print(the_page.decode('utf-8'))
