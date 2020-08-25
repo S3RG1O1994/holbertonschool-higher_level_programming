f01f6f7ec3c736db8ed5f69843fbfb11fda9c432
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-24 20:10:10 -0500

    [Fix]: Delete space between items in the if

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == '__main__':
     rqt = requests.get(sys.argv[1])
-    if rqt.status_code >= 400:
+    if rqt.status_code > 400:
         print('Error code:', rqt.status_code)
     else:
         print(rqt.text)
