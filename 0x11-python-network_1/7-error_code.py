bb4be3e19a6ae1c3d40ca4bfdb61de6ba13a3c1e
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-24 20:01:37 -0500

    [Add]: New script

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This function is print response with id of the site-web'''
+import requests
+import sys
+
+if __name__ == '__main__':
+    rqt = requests.get(sys.argv[1])
+    if rqt.status_code <  400:
+        print(rqt.text)
+    else:
+        print('Error code: {}'.format(rqt.status_code))
