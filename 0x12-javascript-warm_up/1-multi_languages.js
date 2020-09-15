37d5986f5e5cd45b9a58853c2054f99a1a368e28
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 12:24:56 -0500

    [Fix]: semistandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@
 const lenguajeProgramming = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let item;
 for (item in lenguajeProgramming) {
-    console.log(lenguajeProgramming[item]);
+  console.log(lenguajeProgramming[item]);
 }
