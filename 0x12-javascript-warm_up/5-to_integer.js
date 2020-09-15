609ce5bbe887983b1654994f4fa8908fdc5a344d
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 14:52:24 -0500

    [Add]: New task

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+
+const args = process.argv;
+if (isNaN(parseInt(args[2], 10))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(args[2], 10));
+}
