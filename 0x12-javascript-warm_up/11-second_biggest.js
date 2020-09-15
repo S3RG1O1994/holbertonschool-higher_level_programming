2ab63cc05e1ae2ef117570c06a713ef5cb1139d1
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 18:52:45 -0500

    [Add]: New task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,13 @@ const process = require('process');
 
 const args = process.argv;
 if (args[2]) {
-  let myList = args.slice(2);
+  const myList = args.slice(2);
   if (myList.length > 1) {
     console.log(
-      myList.sort(function(a, b){return a - b})[
+      myList.sort(function (a, b) { return a - b; })[
         parseInt(myList.length - 2, 10)
-        ]
-      );
+      ]
+    );
   } else {
     console.log(0);
   }
