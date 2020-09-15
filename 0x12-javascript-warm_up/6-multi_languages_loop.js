a68dc790f0b81c5fede0f26ac672dbc4125c8f83
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 15:11:26 -0500

    [Add]: New task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < words.length; i++) {
-  console.log(words[i]);
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let item = 0; item < list.length; item += 1) {
+  console.log(list[item]);
 }
