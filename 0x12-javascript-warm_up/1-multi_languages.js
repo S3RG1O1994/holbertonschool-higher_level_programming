d145b15adca15c23863d4b311b5a4145e1d2985c
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 13:46:10 -0500

    [Fix]: space after sheebank

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
-const lenguajeProgramming = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const Programming = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let item;
-for (item in lenguajeProgramming) {
-  console.log(lenguajeProgramming[item]);
+for (item in Programming) {
+  console.log(Programming[item]);
 }
