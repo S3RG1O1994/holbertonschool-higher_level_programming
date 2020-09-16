34818acffe1ec3aef682f4d5c42c1a4856a6e722
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-15 20:02:50 -0500

    [Add]: new task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports.callMeMoby = function (x, callMeMoby){
+module.exports.callMeMoby = function (x, callMeMoby) {
   for (let cnt = 0; cnt < x; cnt += 1) {
     callMeMoby();
   }
