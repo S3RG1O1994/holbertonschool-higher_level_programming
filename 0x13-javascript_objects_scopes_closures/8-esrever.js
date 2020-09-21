ee1dd53c0f0ac661a14f78232488435e742fc02d
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-21 12:50:54 -0500

    [Fix]: character x

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  const newList = [];
+  for (let i = 0; i < list.length; i++) {
+    newList.unshift(list[i]);
+  }
+  return newList;
 };
